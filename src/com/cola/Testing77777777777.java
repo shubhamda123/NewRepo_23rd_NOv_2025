bd344c6a0383e27ace87d7d7043be8552c45af73
shubhamda123 <107462870+shubhamda123@users.noreply.github.com>
2025-11-29 19:29:11 +0530

    Remove commented code and extra lines

--- src/com/cola/Testing77777777777.java
+++ src/com/cola/Testing77777777777.java
@@ -3,12 +3,21 @@ package com.cola;
 public class Testing77777777777 {
 
 	public static void main(String[] args) {
-		
-	 System.out.println("testing november");
-		
- //System.out.println("testunfffff");//
-		///--------------------------------------
-	 
+-d
+	d
+	d
+	d
+d
+d	d
+	d
+	d
+d
+	d
+	d
+	d
+d
+	d
+d	d
 	 
 	 
 	 
